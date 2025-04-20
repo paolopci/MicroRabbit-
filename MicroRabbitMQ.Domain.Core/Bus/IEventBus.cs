18c71473eca9c6743d86e790fe6c2699d5b118cf
Paolo Paci <paolopci@yahoo.it>
2025-04-20 23:02:04 +0200

    implemented Interfaces and classes for Domain.Core

---
+++ MicroRabbitMQ.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,17 @@
+﻿using MicroRabbitMQ.Domain.Core.Commands;
+using MicroRabbitMQ.Domain.Core.Events;
+
+
+namespace MicroRabbitMQ.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<T>(T command) where T : Command;
+
+        void Publish<T>(T @event) where T : Event;
+
+        void Subscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>;
+    }
+}
---
+++ MicroRabbitMQ.Domain.Core/Commands/Command.cs
@@ -0,0 +1,16 @@
+﻿using MicroRabbitMQ.Domain.Core.Events;
+
+
+namespace MicroRabbitMQ.Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        
+        public DateTime TimeStamp { get; protected set; }
+
+        protected Command()
+        {
+            TimeStamp = DateTime.Now;
+        }
+    }
+}
